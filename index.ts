ecb0c91add9f45b1ad298b3ee48586d4738546ed
spowll <egoistdestroyer@gmail.com>
2024-11-10 03:50:38 +0100

    Add initial project filess

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
 import { EventsSDK } from "github.com/octarine-public/wrapper/index"
 
-console.log("Hello world!")
+console.log("Heldslo world!")
 EventsSDK.on("GameStarted", () => {
 	console.log("Hellsssssssssssssssssso world!")
 })
+
+console.log("Heldslo world!")
+EventsSDK.on("GameEnded", () => {
+	console.log("Hellsssssssssssssssssso world!")
+})
+
